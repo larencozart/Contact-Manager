c6cbc272786d994789b3a3a8b49bb466e8a527a7
larebear007 <larencozart@gmail.com>
2024-10-06 13:40:22 -0500

    first commit

---
+++ public/javascripts/cm_script.js
@@ -0,0 +1,16 @@
+// imports
+
+class ContactManager {
+  constructor() {
+    // set properties and call necessary methods
+  }
+
+  static initialize() {
+    console.log("CM class is working");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // invoke class;
+  ContactManager.initialize();
+});
