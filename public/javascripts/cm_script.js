2dd5206e97948d6426f186f84d028e88bd146e29
larebear007 <larencozart@gmail.com>
2024-10-07 11:07:38 -0500

    Display all contacts functionality and styling

--- public/javascripts/cm_script.js
+++ public/javascripts/cm_script.js
@@ -1,16 +1,51 @@
 // imports
 
 class ContactManager {
-  constructor() {
+   constructor() {
     // set properties and call necessary methods
+    this.contactsDisplay = document.getElementById('contacts-display');
+    this.tags = [];
+    this.contacts = [];
+
+    console.log("CM class is working");
+    this.displayContacts();
+    
   }
 
   static initialize() {
-    console.log("CM class is working");
+    
+    // this.createUI();
+  }
+
+  async fetchAllContacts() {
+    let response = await fetch("http://localhost:3000/api/contacts");
+    let contacts = await response.json();
+
+    return contacts;
+  }
+
+  displayNoContacts() {
+    let noContacts = document.createElement('p');
+    noContacts.classList.add("no-contacts");
+    this.contactsDisplay.appendChild(noContacts);
+    noContacts.textContent = "No contacts yet. Go ahead and add one!";
+  }
+
+  async displayContacts() {
+    let contacts = await this.fetchAllContacts();
+
+    if (contacts.length < 1) {
+      this.displayNoContacts();
+    } else {
+      // handlebars baby
+      let contactHandlebar = document.getElementById("contact-template");
+      let contactTemplate = Handlebars.compile((contactHandlebar).innerHTML);
+      this.contactsDisplay.innerHTML = contactTemplate({contacts: contacts});
+    }
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   // invoke class;
-  ContactManager.initialize();
+  let contactManager = new ContactManager();
 });
